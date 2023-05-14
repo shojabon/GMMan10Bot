314a162fcf8f717e5245a593eaa1c6076489ef85
sho <shojabon@gmail.com>
2023-05-14 18:51:02 +0900

    added initial components to Man10BotKnowledge

--- GMMan10Bot/data_class/Man10BotKnowledge/__init__.py
+++ GMMan10Bot/data_class/Man10BotKnowledge/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from enum import Enum
 from typing import TYPE_CHECKING
 import uuid
 
@@ -12,6 +13,13 @@ class Man10BotKnowledge:
     def __init__(self):
         self.id = self.generate_id()
         self.application: Man10BotApplication | None = None
+        self.anchor_questions = []
+        self.anchor_keywords = []
+        self.data_title = ""
 
+    def is_allowed_to_use(self, user_info, message, search_metadata) -> bool:
+        return True
+    def get_data(self, user_info, message, search_metadata) -> str:
+        pass
     def generate_id(self):
         return str(uuid.uuid4())
